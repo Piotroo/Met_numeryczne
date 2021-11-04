f0814e630ff7b80e3ca39229584919b634625077
Piotroo <89666610+Piotroo@users.noreply.github.com>
2021-11-04 19:01:54 +0100

    Add files via upload

--- KOD/MRS/MRS.m
+++ KOD/MRS/MRS.m
@@ -1,34 +1,33 @@
-%--skrypt do symulacji obliczenia pola potencjalu metoda roznicskonczonych
-%-- rozwiazanierownania Laplace'a
+%--skrypt do symulacji obliczenia pola potencjalu metoda roznic skonczonych
+%-- rozwiazanie rownania Laplace'a
 clear; clc; format shortg;
-Nx = 30; Ny = 30; % rozmiar siatki
-Lx = 1; Ly = 1; % wielkość (np. temperatura)
-x = linspace(0, Lx, Nx); % podział wielkości na ilośc kroków (rozmiar na 
-rowne cześci) na x 
-y = linspace(0, Ly, Ny); % podział wielkości na ilośc kroków (rozmiar na 
-rowne czesci) na y
-dx = x(2) - x(1); dy = y(2) - y(1); % policzenie wielkości różnic pomiędzy 
-krokami w osi x oraz y
-N = Nx*Ny; M = zeros(N,N); % utworzenie macierzy o wymiarach Nx / Ny ;
-B = zeros(N,1); % wektor
-fori =2:Nx-1 % pętla
+
+Nx = 30; Ny = 30;                       % rozmiar siatki
+Lx = 1; Ly = 1;                         % wielkość (np. temperatura)
+x = linspace(0, Lx, Nx);                % podział wielkości na ilośc kroków (rozmiar na rowne cześci) na x 
+y = linspace(0, Ly, Ny);                % podział wielkości na ilośc kroków (rozmiar na rowne czesci) na y
+dx = x(2) - x(1); dy = y(2) - y(1);     % policzenie wielkości różnic pomiędzy krokami w osi x oraz y
+N = Nx*Ny; M = zeros(N,N);              % utworzenie macierzy o wymiarach Nx / Ny ;
+B = zeros(N,1);                         % wektor
+for i =2:Nx-1                           % pętla
 for j =2 :Ny-1
-n = i+(j-1)*Nx; %--indexij na liniowy
- M(n,n) = -4; % wpisanie w miejsce 4
- M(n,n-1) = 1; % wpisanie w sąsiada poprzedzającego 1
- M(n,n+1) = 1; % =/= następującego 1
- M(n, n-Nx) = 1; % =/= wiersz wyżej
- M(n, n+Nx) = 1; % =/= wiersz niżej
- B(n, 1) = 0; %--tu nie potrzebne w przypPoisonasourceterms
+n = i+(j-1)*Nx;                         %--indexij na liniowy
+ M(n,n) = -4;                           % wpisanie w miejsce 4
+ M(n,n-1) = 1;                          % wpisanie w sąsiada poprzedzającego 1
+ M(n,n+1) = 1;                          % =/= następującego 1
+ M(n, n-Nx) = 1;                        % =/= wiersz wyżej
+ M(n, n+Nx) = 1;                        % =/= wiersz niżej
+ B(n, 1) = 0;                           % tu nie potrzebne w przyp Poisona ??
 end
 end
 %% Warunki brzegowe
+
 %lewa krawędź phi=y
 i = 1;
 for j = 1:Ny
  n = i+(j-1)*Nx; 
- M(n,n) = 1; %- zastepuje -4 przez jeden tam gdzie jest war brzegowy
-B(n,1) = y(j); % B(n,1) = 1;
+ M(n,n) = 1;                    %- zastepuje -4 przez jeden tam gdzie jest war brzegowy
+B(n,1) = y(j);                  % B(n,1) = 1;
 end
 % prawa phi = 1-y
 i = Nx;
@@ -36,17 +35,17 @@ for j = 1:Ny
  n = i+(j-1)*Nx; 
  M(n,n) = 1; 
  B(n,1) = 1-y(j);
-End
+end
 % gora phi = 1-x
 j = Ny;
-fori = 1:Nx
+for i = 1:Nx
  n = i+(j-1)*Nx; 
 M(n,n) = 1; 
  B(n,1) = 1-x(i);
 end
 % dol phi = 1-x
 j = 1;
-fori = 1:Nx
+for i = 1:Nx
 n = i+(j-1)*Nx; 
  M(n,n) = 1; 
  B(n,1) = x(i);
@@ -56,7 +55,7 @@ end
 phi_vec = M\B;
 %%
 phi = zeros(Nx,Ny);
-fori =1:Nx
+for i =1:Nx
 for j=1:Ny
 n = i+(j-1)*Nx;
 phi(i,j) = phi_vec(n);
@@ -65,9 +64,9 @@ end
 %
 surf(x,y,phi); xlabel('x'); ylabel('y');
 title('Rozkladpotencjalu skalarnego');
-%% -- obliczenie predkoscipzeplywu
+%% obliczenie predkosci przeplywu
 u = zeros(Nx, Ny); v = zeros(Nx, Ny);
-fori = 2:Nx-1
+for i = 2:Nx-1
 for j = 2:Ny-1
 u(i,j) = -(phi(i+1,j) - phi(i-1, j))/(2*dx);
  v(i,j) = - (phi(i, j+1) - phi(i,j-1))/(2*dy);
