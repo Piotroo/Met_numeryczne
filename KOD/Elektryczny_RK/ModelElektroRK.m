16b9f33c908920d82e37351324e85b6e738237d5
Piotroo <89666610+Piotroo@users.noreply.github.com>
2021-11-04 15:18:29 +0100

    Add files via upload

--- KOD/Elektryczny_RK/ModelElektroRK.m
+++ KOD/Elektryczny_RK/ModelElektroRK.m
@@ -1,20 +1,32 @@
 function [fprime] = ModelElektroRK (y, tt)
 % t - czas, y- wartość wektoru w obecnym punkcie
 % funckcja z modelem matematycznym dla układu elektrycznego
+
 global C1 C2 R1 R2 L1 L2 u
+
 % małe macierze
-L = [L1 0; ... % mała macierz L
- 0, L2]; 
-C = [ ((1/C1)+(1/C2)), -(1/C2); ... % mała macierz C
- -(1/C2), (1/C2)]; 
-R = [R1, 0;... % mała macierz R
- 0, R2 ];
-%% Następnym krokiem jest utworzenie dużych macierzy
+L = [   L1 0;                           % mała macierz L
+        0, L2]; 
+    
+C = [ ((1/C1)+(1/C2)),    -(1/C2);      % mała macierz C
+      -(1/C2),             (1/C2)];
+    
+R = [R1,    0;                          % mała macierz R
+     0,     R2 ];
+
+ 
 % duże macierze
-A = [L, zeros(2); zeros(2), eye(2)]; % macierz A złożona z macierzy L oraz 
-zerowych i jednostkowych
-B = [ R, C; -eye(2), zeros(2)]; %macierz B złożona z macierzy R oraz C 
-Q = [u;0]; % Napięcie przyłożone do układu
-U = [Q; zeros(2,1)]; % wektor siły U
-fprime = A \ (U - B*y) ; % 
+A = [L,         zeros(2); 
+     zeros(2),  eye(2)];                % macierz A złożona z macierzy L oraz zerowych i jednostkowych
+ 
+B = [ R,        C; 
+     -eye(2),   zeros(2)];              % macierz B złożona z macierzy R oraz C 
+ 
+Q = [u;
+     0];                                % Napięcie przyłożone do układu
+ 
+U = [Q; 
+     zeros(2,1)];                       % wektor siły U
+ 
+fprime = A \ (U - B*y) ;  
 end
