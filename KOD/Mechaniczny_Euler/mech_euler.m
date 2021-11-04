4ca7fdebd53ee72c3a085e452b1313cde8c4c9cb
Piotroo <89666610+Piotroo@users.noreply.github.com>
2021-11-04 13:54:04 +0100

    Add files via upload

--- KOD/Mechaniczny_Euler/mech_euler.m
+++ KOD/Mechaniczny_Euler/mech_euler.m
@@ -1,48 +1,63 @@
-
 %% Model Mechaniczny z wykorzystaniem metody Eulera
 clear; clc; 
 %dane 
-k1 = 1; k2 = 2; k3 = 3; %sprężyny
-D1 = 5; % tłumienie
-m1 = 1 ; m2 = 2; m3 = 3; % masy
+k1 = 1; k2 = 2; k3 = 3;     %sprężyny
+D1 = 5;                     % tłumienie
+m1 = 1 ; m2 = 2; m3 = 3;    % masy
 
-% wymuszenie napięcia (skokowe)
-u = 10; % siła stała w chwili przyłożenia = t0
+% wymuszenie (skokowe)
+f = 10; % siła stała w chwili przyłożenia = t0
 %% dziedzina czasu
-t0 = 0; tf = 50; dt = 1e-4; t = t0:dt:tf;
+t0 = 0; 
+tf = 100; 
+dt = 1e-4; 
+t  = t0:dt:tf;
 
 %% tworzenie małych macierzy
 
 % małe macierze
 M = [   m1, 0, 0; ...
-        0, m2, 0; ...               % mała macierz M
+        0, m2, 0; ...               % mała macierz M masy
         0, 0, m3] ; 
 
 K = [   k1+k2+k3, -k2, -k3 ; ...  
-        -k2, k2, 0 ; ...            % mała macierz K
+        -k2, k2, 0 ; ...            % mała macierz K wsp. sprezystosci
         -k3, 0, k3 ] ; 
 
 D = [   0 , 0, 0; ...
-        0 , D1, 0; ...              % mała macierz D
+        0 , D1, 0; ...              % mała macierz D tarcie 
         0 , 0, 0] ;
 
     
 
-    % duże macierze
-A = [   M, zeros(3); zeros(3), eye(3)]; % macierz A złożona z macierzy m oraz 
-                                        % zerowych i jednostkowych
+% duże macierze
+A = [   M,          zeros(3);           % macierz A(złożona z macierzy m, zerowych i eye(3) jednostkowa)
+        zeros(3),   eye(3)];             
                                         
-B = [ D, K; -eye(3), zeros(3)];         %macierz B złożona z macierzy D oraz K 
+B = [ D, K; -eye(3), zeros(3)];         % macierz B (tarciem sprezystosc)  
 
-Q = [u;0;0];                            % Siła przyłożona do układu
-U = [Q; zeros(3,1)];                    % wektor siły U
-                                        %--definicja warunkowpoczatkowychY0 = zeros(6,1);
-                                        %-- alokacja pamieci--
-wyn = zeros(6,max(size(t))); wyn(:,1) = Y0;
-%% -- rozwiazanie met Eulera - wykazuje niestabilnosc
+Q = [0;                                 % macierz Q
+     f;                                 % Siła przyłożona do układu
+     0];
+ 
+
+U = [Q; 
+     zeros(3,1)];                       % wektor siły U (w pdf F)
+
+                                        
+Y0 = zeros(6,1);                        % definicja warunkow poczatkowych
+
+
+wyn = zeros(6,max(size(t)));            % alokacja pamieci
+
+wyn(:,1) = Y0;                          % cały rząd 1 kolumny
+%% rozwiazanie met Eulera - wykazuje niestabilnosc
+
 for k = 1:1:max(size(t))-1
 wyn(:,k+1) = wyn(:,k) + A\(U - B*wyn(:,k))*dt;
 end
+
+
 subplot(211);
 plot(t,wyn(1,:),'-blue',t,wyn(2,:),'-green',t,wyn(3,:),'-red')
 xlabel ('czas [t]')
